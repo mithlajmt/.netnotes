2b5c53487c45f3830cabce8eafab3f865304e198
mithlajmatta <144442116+mithlajmatta@users.noreply.github.com>
2024-08-31 22:42:19 +0530

    testing git

---
+++ Controllers/tester.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")] //The Route attribute in the controller specifies the base route for all the endpoints in this controller.
+    //The[controller] placeholder is replaced with the name of the controller(minus the Controller suffix). For tester, the base route would be api/MyTest. 
+   
+    [ApiController]////mark the controller class as 
+    //an api controller indicating its responsible for handle http requests
+    //ffect: Enables features like automatic model validation, route template binding, and problem details for error handling. It helps ensure a consistent and predictable API behavior.
+    public class tester : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("vannedaa ninte okke appan");
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build(); //configures the req res 
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run(); //to run our server and initiaate http req process pipeline
